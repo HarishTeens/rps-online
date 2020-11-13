f45ce6e76ad2b0a2aa5f7eca7b1828344ce701ea
HarishTeens <arishh2@gmail.com>
2020-11-13 17:31:39 +0530

    app.js update boilerplate

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const app=require('express');
 const socket=require('socket.io');
+const randomstring=require('randomstring');
 
 const express=app();
 
@@ -9,8 +10,50 @@ const server=express.listen(4000,()=>{
 
 express.use(app.static('public'));
 
-express.get("/",(req,res)=>{
-    res.render("index.html");
-})
-
 const io=socket(server);
+
+//ALL player info
+let players={};
+//GAME VARIABLES
+let choice1="",choice2="";
+
+
+
+
+
+
+
+
+//Function to calculate winner
+const getWinner=(p, c)=>  {
+    if (p === c) {
+        return "draw";
+    } else if (p === "Rock") {
+        if (c === "Paper") {
+            return false;
+        } else {
+            return true;
+        }
+    } else if (p === "Paper") {
+        if (c === "Scissor") {
+            return false;
+        } else {
+            return true;
+        }
+    } else if (p === "Scissor") {
+        if (c === "Rock") {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+//Function to do executed after gettin both choices
+const result=(roomID)=> {
+    var winner = getWinner(choice1, choice2);
+    io.sockets.to(roomID).emit("result", {
+        winner: winner
+    });
+    choice1 = "";
+    choice2 = "";
+}
