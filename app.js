dd8e11db5e50aeab80c020560c439ba7656196b5
HarishTeens <arishh2@gmail.com>
2020-11-19 10:37:55 +0530

    final version

--- app.js
+++ app.js
@@ -17,12 +17,41 @@ let players={};
 //GAME VARIABLES
 let choice1="",choice2="";
 
+io.on("connection",(socket)=>{
+    console.log("connection established");
 
+    //Create Game Listener
+    socket.on("createGame",(data)=>{
+        const roomID=randomstring.generate({length: 4});       
+        socket.join(roomID);        
+        players[roomID]=data.name;
+        socket.emit("newGame",{roomID:roomID});
+    })
 
+    //Join Game Listener
+    socket.on("joinGame",(data)=>{        
+        socket.join(data.roomID);
+        socket.to(data.roomID).emit("player2Joined",{p2name: data.name,p1name:players[data.roomID]});
+        socket.emit("player1Joined",{p2name:players[data.roomID],p1name:data.name});
+    })
 
-
-
-
+    //Listener to Player 1's Choice
+    socket.on("choice1", (data)=> {
+        choice1 = data.choice;
+        console.log(choice1, choice2);
+        if (choice2 != "") {
+            result(data.roomID);
+        }
+    });
+    //Listener to Player 2's Choice
+    socket.on("choice2", (data)=> {
+        choice2 = data.choice;
+        console.log(choice1, choice2);
+        if (choice1 != "") {
+            result(data.roomID);
+        }
+    });
+})
 
 //Function to calculate winner
 const getWinner=(p, c)=>  {
@@ -47,4 +76,13 @@ const getWinner=(p, c)=>  {
             return true;
         }
     }
+}
+//Function to do executed after gettin both choices
+const result=(roomID)=> {
+    var winner = getWinner(choice1, choice2);
+    io.sockets.to(roomID).emit("result", {
+        winner: winner
+    });
+    choice1 = "";
+    choice2 = "";
 }
--- public/game.js
+++ public/game.js
@@ -2,3 +2,75 @@ const socket = io.connect("http://localhost:4000");
 
 let firstPlayer=false;
 let roomID;
+
+//Create Game Event Emitter
+$(".createBtn").click(function(){
+    firstPlayer=true;
+    const playerName=$("input[name=p1name").val();
+    socket.emit('createGame',{name:playerName});
+})
+
+//New Game Created Listener
+socket.on("newGame",(data)=>{
+    $(".newRoom").hide();
+    $(".joinRoom").hide();
+    $("#message").html("Waiting for player 2, room ID is "+data.roomID).show();
+    roomID=data.roomID;
+})
+
+//Join Game Event Emitter
+$(".joinBtn").click(function(){
+    const playerName=$("input[name=p2name").val();
+    roomID=$("input[name=roomID").val();    
+    socket.emit('joinGame',{
+        name:playerName,
+        roomID:roomID
+    });
+})
+
+//Player 2 Joined
+socket.on("player2Joined",(data)=>{
+    transition(data)  ;
+  })
+  
+//Player 1 Joined
+socket.on("player1Joined",(data)=>{
+    transition(data)  ;
+})
+
+//Select Choice
+$(".controls button").click(function (){
+    const choice=$(this).html().trim();
+    const choiceEvent=firstPlayer?"choice1":"choice2";
+    socket.emit(choiceEvent,{
+        choice: choice,
+        roomID:roomID
+    });
+})
+
+//Result Event Listener
+socket.on("result",(data)=>{
+    if(data.winner=="draw"){
+        $("#message").html("It's a draw!");
+    }else{
+        updateDOM(firstPlayer==data.winner?"player1":"player2");
+    }
+})
+
+const updateDOM=(player)=>{
+    const playerDOM=$("."+player+" span");
+    const prevScore=parseInt(playerDOM.html().trim());
+    playerDOM.html(prevScore+1);
+    const winnerName=$("."+player+" .name").html().trim();
+    $("#message").html(winnerName+" scored a point!");
+}
+
+const transition=(data)=>{
+    $(".newRoom").hide();
+    $(".joinRoom").hide();
+    $(".leaderboard").show();
+    $(".controls").show();
+    $(".player1 .name").html(data.p1name);
+    $(".player2 .name").html(data.p2name);
+    $("#message").html(data.p2name+" is here!").show();
+}
